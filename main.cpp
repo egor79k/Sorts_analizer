cbff8424997ce8018ddaac5e135b62947f413735
egor79k <egor79k@yandex.ru>
2020-09-09 22:20:33 +0300

    Commit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+const int Buttons_num = 4;
+
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1000, 700), "Sorts analizer");
-
-	int color = 0;
+	/*sf::RectangleShape buttons[Buttons_num] = {	sf::RectangleShape (sf::Vector2f (100, 30)),
+												sf::RectangleShape (sf::Vector2f (100, 30)),
+												sf::RectangleShape (sf::Vector2f (100, 30)),
+												sf::RectangleShape (sf::Vector2f (100, 30)),};
+*/
+	sf::Font font;
+	font.loadFromFile ("Country_Western_Regular.ttf");
+	sf::Text text ("Bubble", font, 20);
+	text.setColor (sf::Color::Red);
 	
 	while (window.isOpen())
 	{
 		window.clear (sf::Color (255, 255, 255));
-
-		sf::Vector2i mouse_pos = sf::Mouse::getPosition (window);
-
+		window.draw (text);
+/*
 		sf::RectangleShape button_1 (sf::Vector2f (100, 30));
 		button_1.setPosition (50, 400);
 		button_1.setFillColor (sf::Color (0, 255, 0));
 		window.draw (button_1);
-
+*/
 
 		sf::Event event;
 		
@@ -29,9 +37,10 @@ int main()
 
 			if (event.type == sf::Event::MouseButtonPressed && static_cast<int> (event.key.code) == static_cast<int> (sf::Mouse::Left))
 			{
+				sf::Vector2i mouse_pos = sf::Mouse::getPosition (window);
+/*
 				if (button_1.getGlobalBounds ().contains (mouse_pos.x, mouse_pos.y))
-					std::cout << "Button pressed!\n";
-				//color = (color + 10) % 255;
+					std::cout << "Button pressed!\n";*/
 			}
 		}
 
