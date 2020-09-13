4feb24b0239febf77e3fc3fba4adc15fcedb94ef
egor79k <egor79k@yandex.ru>
2020-09-13 12:41:21 +0300

    Graph class created

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 const int Buttons_num = 4;
 
@@ -37,48 +38,51 @@ public:
 class Graph
 {
 private:
-	const int x_pos;
-	const int y_pos;
-	int points_num;
-	float scale;
+	const float x_pos;
+	const float y_pos;
+	const float scale;
 	float max_x;
 	float max_y;
+	float min_x;
+	float min_y;
+	uint32_t points_num;
 	sf::Color color;
-	sf::RectangleShape Ox;
-	sf::RectangleShape Oy;
 	sf::VertexArray graph;
 
 
 public:
-	Graph (int x, int y, float sc, const sf::Color &col) :
+	Graph (float x, float y, float sc = 1, const sf::Color &col = sf::Color::Black) :
 		x_pos (x),
 		y_pos (y),
-		points_num (0),
 		scale (sc),
-		max_x (0),
-		max_y (0),
+		max_x (x),
+		max_y (y),
+		min_x (x),
+		min_y (y),
+		points_num (0),
 		color (col),
-		Ox (sf::Vector2f (100,1)),
-		Oy (sf::Vector2f (1, 100)),
 		graph (sf::VertexArray (sf::LineStrip))
-	{
-		Ox.setPosition (x, y + 100);
-		Oy.setPosition (x, y);
-		Ox.setFillColor (sf::Color::Black);
-		Oy.setFillColor (sf::Color::Black);
-	}
+	{}
 
-	void add_point (int x, int y)
+	void add_point (float x, float y)
 	{
-		graph.append (sf::Vector2f (x_pos + x / scale, y_pos + 100 - y / scale));
+		graph.append (sf::Vector2f (x_pos + x / scale, y_pos - y / scale));
+		if (graph[points_num].position.x > max_x) max_x = graph[points_num].position.x;
+		if (graph[points_num].position.y < max_y) max_y = graph[points_num].position.y;
+		if (graph[points_num].position.x < min_x) min_x = graph[points_num].position.x;
+		if (graph[points_num].position.y > min_y) min_y = graph[points_num].position.y;
 		graph[points_num].color = color;
 		++points_num;
 	}
 
 	void draw (sf::RenderWindow &window)
 	{
-		sf::RectangleShape Ox (Vector2f (max_point, 1));
-		sf::RectangleShape Oy;
+		sf::RectangleShape Ox (sf::Vector2f (max_x - min_x, 1));
+		sf::RectangleShape Oy (sf::Vector2f (1, min_y - max_y));
+		Ox.setPosition (min_x, y_pos);
+		Oy.setPosition (x_pos, max_y);
+		Ox.setFillColor (sf::Color::Black);
+		Oy.setFillColor (sf::Color::Black);
 		window.draw (Ox);
 		window.draw (Oy);
 		window.draw (graph);
@@ -89,19 +93,20 @@ public:
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 200), "Sorts analizer");
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Sorts analizer");
 
-	Graph gr (5, 5, 0.5, sf::Color::Blue);
+	Graph gr (100, 100, 0.1, sf::Color::Blue);
 
 	//Button button_bubble ("Bubble", sf::Color (150, 0, 0), 0, 0);
 
-	
-	
+	for (float f = -10; f < 10; f += 0.1)
+		gr.add_point (f,  sqrt (99 - f * f));
+	/*
 	gr.add_point (0, 0);
-	gr.add_point (10, 30);
-	gr.add_point (20, 40);
-	gr.add_point (30, 30);
-	gr.add_point (40, 0);
+	gr.add_point (10, 0);
+	gr.add_point (20, 0);
+	gr.add_point (30, 0);
+	gr.add_point (40, 0);*/
 /*
 	func[0].color = sf::Color::Blue;
 	func[1].color = sf::Color::Blue;
